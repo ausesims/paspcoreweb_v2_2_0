76e9953fe8b1818ed3dbf8a1fa47846c42831587
Ause Sims <ause.sims@outlook.com>
2019-07-17 14:54:51 -0400

    - Visitors will now notice a Site Cookie and Privacy Alert when navigating to this site. This alert contains a brief explanation of cookies, with a “Cookies and Privacy Information” button for more information as well as an “Accept” button. The banner cannot be cleared until the visitor or user clicks on the “Accept” button. - A “Not Agree” button has been added to the Site Cookie and Privacy Alert. When visitors clicking on this button will be navigated to the Google search page at http://www.google.com - The Cookies and Privacy page on the site has been updated to the latest current information to keep with website compliancy. - The “Privacy” navigation link button is now available on the site Navigation bar. This was removed during migration from .NET to .NET Core because the Cookie and Privacy Policy was not established during that time. - The “container” class element on the main layout page for the site was replaced with the “container-fluid” class element to give full width paragraph when needed. - Buttons have now been added to the “Change Log” page in the following format: -  “Change Log: MM/DD/YYYY – Change Set: #”. This was a requested from some users in the Visual Studio Education community who uses the change set numbers to keep track of changes for educational purposes. When visitors, or registered users click on the button information will be displayed for the changes made to the site.

---
+++ aspcoreweb_blog_v2_2_0/aspcoreweb_blog_v2_2_0/Views/Shared/PageContent/_PrivacyPolicyPartial.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspcoreweb_blog_v2_2_0.Views.Shared.PageContent
+{
+    public class _PrivacyPolicyPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
