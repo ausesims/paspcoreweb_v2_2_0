f0cfb4d6384b4dbd3edb332974a5bdf45e1a3a83
Ause Sims <ause.sims@icloud.com>
2019-09-18 06:54:53 -0400

    08/08/2019 - The Microsoft.AspNetCore.Razor.Design package was updated from 2.1.2 to the latest stable version of 2.2.0. This package contains MSBuild support for the Razor markup syntax, which is used for server-side logic to web pages.

--- aspcoreweb_blog_v2_2_0/aspcoreweb_blog_v2_2_0/Controllers/HomeController.cs
+++ aspcoreweb_blog_v2_2_0/aspcoreweb_blog_v2_2_0/Controllers/HomeController.cs
@@ -17,46 +17,61 @@ namespace aspcoreweb_blog_v2_2_0.Controllers
 
         public ActionResult Projects()
         {
-            ViewBag.Message = "My projects list";
+            ViewData["Projects Title"] = "PROJECT LISTING";
+
+            ViewData["Projects Message"] = "This page list projects, which are current, on-going " +
+                "or have been completed. All public projects are accessable via GitHub and private" +
+                " projects are accessable via Azure DevOps (formerly Team Foundation Services)";
+
+            ViewData["Projects Instructions"] = "Click on a button to view the project information.";            
 
             return View();
         }
 
         public ActionResult Portfolio()
         {
-            ViewBag.Message = "My Portfolo";
+            ViewData["Portfolio Title"] = "PORTFOLIO";
+
+            ViewData["Portfolio Message"] = "To be implimented on 09/21/2019";
 
             return View();
         }
 
         public ActionResult Sites()
         {
-            ViewBag.Message = "My website listings page.";
+            ViewBag.Message = "EXTERNAL TECHNOLOGY SITES";
 
             return View();
         }
 
         public ActionResult Blog()
         {
+            ViewData["Blog Title"] = "BLOG";
+            ViewData["Blog Message"] = "To be implmented on 09/20/2019.";
+
             return View();
         }
 
         public ActionResult Art()
         {
+            ViewData["Art Title"] = "ART";
+            ViewData["Art Message"] = "To be implmented on 09/19/2019";
+
             return View();
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Title"] = "Contact Information for Ause L. Sims";
-            ViewData["Message"] = "You can use one of several various methods " +
+            ViewData["Contact Title"] = "CONTACT INFORMATION";
+
+            ViewData["Contact Message"] = "You can use one of several various methods " +
                 "listed below to contact me. For online chat hours please view" +
                 " the schedule listed at the bottom of the page.";
 
@@ -65,7 +80,15 @@ namespace aspcoreweb_blog_v2_2_0.Controllers
 
         public ActionResult ChangeLog()
         {
-            ViewBag.Message = "Site change-log actions which warrant, changes, additions, or corrections to the stie.";
+            ViewData["Change Log Title"] = "CHANGE LOG";
+
+            ViewData["Change Log Message"] = "The site Change Log which warrants, changes, additions, or " +
+                "corrections to the stie and it's content. Site changes are preformed on a monthly basis" +
+                " with the exception of emgerency changes. An alert pop-up will be present on this page when " +
+                "a emgerency change is in progress. All public project changes are posted to GitHub and all" +
+                " private project changes are posted to Azure DevOps";
+
+            ViewData["Change Log Instructions"] = "Click on a button below to list the assoicated change";
 
             return View();
         }
